4e96b7bda60c18cb3af688eb866768ffa9ebba03
ashmakadar <44708592+ashmakadar@users.noreply.github.com>
2018-11-02 23:22:57 +0530

    Add files via upload

---
+++ pallindrome.py
@@ -0,0 +1,6 @@
+usersrting=input("enter a string:")
+rev_userstring=reversed(userstring)
+if list(userstring)==list(rev_userstring):
+    print("it is palindrome")
+else:
+    print("it is not pallindrome")
